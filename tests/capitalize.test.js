cb18efde575283339517ba30e5866f0fa98a2460
Qupol Two Times <ilya.v-i-n-o@yandex.ru>
2023-03-13 18:30:07 +0300

    make step 2

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,9 +1,16 @@
+import { strict as assert } from 'node:assert';
 import capitalize from '../src/capitalize.js';
 
+assert.equal(capitalize('hello'), 'Hello');
+
+assert.equal(capitalize(''), '');
+
+console.log('Все тесты пройдены!');
+
+/*
 if (capitalize('hello') !== 'Hello') {
   throw new Error('Функция работает неверно!');
 }
 if (capitalize('') !== '') {
   throw new Error('Функция работает неверно!');
-}
-console.log('Все тесты пройдены!');
+} */
